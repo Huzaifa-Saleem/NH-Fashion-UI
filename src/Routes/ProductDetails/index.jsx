15070c0f10aca96a0f4da206163f24da767a4bd3
Nisha-hayat786 <nisha6895540@gmail.com>
2022-08-01 19:11:15 +0500

    product details updated

--- src/Routes/ProductDetails/index.jsx
+++ src/Routes/ProductDetails/index.jsx
@@ -16,7 +16,7 @@ const ProductDetails = () => {
 
   useEffect(() => {
     const getProducts = async () => {
-      const res = await axios.create.get('http://localhost:4000/api/products/find/' + id)
+      const res = await axios.get('http://localhost:4000/api/products')
       setPosts(res.data);     
     };
 
